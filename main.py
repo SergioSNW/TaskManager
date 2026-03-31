e764fb9e0c12f99e35b81f9800348895127eddf4
SergioSNW <synssn@gmail.com>
2026-03-31 18:59:18 +0100

    persistence implemented

--- main.py
+++ main.py
@@ -15,36 +15,41 @@ def main():
     while True:
     
         print_menu()
+        
+        try:
     
-        choice = input("Choose an option: ")
-    
-        match choice:
-            case "1":
-                description = input("Enter task description: ")
-                manager.add_task(description)
-            
-            case "2":
-                manager.list_tasks()
+            choice = int(input("Choose an option: "))
+        
+            match choice:
+                case 1:
+                    description = input("Enter task description: ")
+                    manager.add_task(description)
                 
-            case "3":
-                id = input("Enter task ID to complete: ")
-                if id.isdigit():
-                    manager.complete_task(int(id))
-                else:
-                    print("Invalid ID. Please enter a numeric value.")
+                case 2:
+                    manager.list_tasks()
                     
-            case "4":
-                id = input("Enter task ID to delete: ")
-                if id.isdigit():
-                    manager.delete_task(int(id))
-                else:                    
-                    print("Invalid ID. Please enter a numeric value.")
+                case 3:
+                    id = input("Enter task ID to complete: ")
+                    if id.isdigit():
+                        manager.complete_task(int(id))
+                    else:
+                        print("Invalid ID. Please enter a numeric value.")
+                        
+                case 4:
+                    id = input("Enter task ID to delete: ")
+                    if id.isdigit():
+                        manager.delete_task(int(id))
+                    else:                    
+                        print("Invalid ID. Please enter a numeric value.")
+                    
+                case 5:
+                    print("Exiting the Task Manager. Goodbye!")
+                    break
+                case _: # Default case for Default in other languages
+                    print("Invalid option. Please choose a valid option.")
                 
-            case "5":
-                print("Exiting the Task Manager. Goodbye!")
-                break
-            case _: # Default case for Default in other languages
-                print("Invalid option. Please choose a valid option.")
-            
+        except ValueError:
+            print("Invalid input. Please enter a numeric value.")
+
 if __name__ == "__main__": # If the file is named main.py, this code will run. Being the same name as the function is just a convention, it can be named differently.
     main()
--- task_manager.py
+++ task_manager.py
@@ -1,3 +1,5 @@
+import json
+
 class Task:
     
     def __init__(self, id, description, completed=False):
@@ -12,15 +14,20 @@ class Task:
     
 class TaskManager:
     
+    # Constantes en python se suelen escribir en mayusculas, aunque no es obligatorio. Es una convención.
+    FILENAME = "tasks.json"
+    
     def __init__(self):
         self._tasks = []
         self._next_id = 1
+        self.load_tasks()
         
     def add_task(self, description):
         task = Task(self._next_id, description)
         self._tasks.append(task)
         self._next_id += 1
         print(f"Task added: {description}")
+        self.save_tasks()
     
     def list_tasks(self):
         if not self._tasks:
@@ -34,7 +41,8 @@ class TaskManager:
         for task in self._tasks:
             if id == task.id:
                 task.completed = True
-                print(f"Task #{id} marked as completed.")
+                print(f"Task #{id} marked as completed.[✓]")
+                self.save_tasks()
                 return
         print(f"Task with id #{id} not found.")
     
@@ -43,5 +51,26 @@ class TaskManager:
             if task.id == id:
                 self._tasks.remove(task)
                 print(f"Task #{id} deleted.")
+                self.save_tasks()
                 return
         print(f"Task with id #{id} not found.")
+        
+    def load_tasks(self):
+        try:
+            # With open es una forma de manejar archivos que asegura que el archivo se cierre correctamente después de su uso, incluso si ocurre un error durante la operación. Es una buena práctica para evitar fugas de recursos.
+            # El modo "r" indica que el archivo se abrirá en modo de lectura. Si el archivo no existe, se lanzará una excepción FileNotFoundError, que es manejada por el bloque except.
+            with open(self.FILENAME, "r") as file:
+                data = json.load(file)
+                self._tasks = [Task(item["id"], item["description"], item["completed"]) for item in data]
+                if self._tasks:
+                    self._next_id = self._tasks[-1].id + 1
+                else:
+                    self._next_id = 1
+                    
+        except FileNotFoundError:
+            self._tasks = []
+    
+    def save_tasks(self):
+        with open(self.FILENAME, "w") as file:
+            # La función json.dump() se utiliza para escribir datos en un archivo en formato JSON. En este caso, se está escribiendo una lista de diccionarios, donde cada diccionario representa una tarea con sus atributos (id, description, completed). La comprensión de listas se utiliza para crear esta lista de diccionarios a partir de la lista de objetos Task.
+            json.dump([{"id": task.id, "description": task.description, "completed": task.completed} for task in self._tasks], file, indent=4)
